fd51089a36e1c7f2905617033f3226e2c59df11c
Andres Caceres <8844096+pinkfloydsito@users.noreply.github.com>
2024-11-23 00:47:18 +0200

    fix(commodities): Fixing the dbt model
    the dbt model was inconsistent, the country was wrongly fetched

--- dbt/models/core/localities.sql
+++ dbt/models/core/localities.sql
@@ -3,7 +3,7 @@
 SELECT
     DISTINCT adm1_id AS id,
     adm1_name AS name,
-    adm0_id AS country_id
+    (select id from countries where name={{ source('core', 'raw_wfp') }}.adm0_name limit 1) AS country_id
 FROM {{ source('core', 'raw_wfp') }}
 WHERE adm1_id IS NOT NULL AND adm1_name IS NOT NULL
 ORDER BY name
