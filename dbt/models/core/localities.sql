18ca38676c5bd855b6c1b91b6bac16b1c56573ea
Andres Caceres <8844096+pinkfloydsito@users.noreply.github.com>
2024-11-24 11:50:53 +0200

    feat(geolocation): Geolocation fetched for every locality
    DAG created used to fetch the geolocation. Fixed models countries and localities

--- dbt/models/core/localities.sql
+++ dbt/models/core/localities.sql
@@ -2,8 +2,30 @@
 
 SELECT
     DISTINCT adm1_id AS id,
-    adm1_name AS name,
-    (select id from countries where name={{ source('core', 'raw_wfp') }}.adm0_name limit 1) AS country_id
+    TRIM(
+        REGEXP_REPLACE(
+            REGEXP_REPLACE(adm1_name, '[^a-zA-Z0-9\s''\-\(\)]', '', 'g'),
+            '\s+', ' ', 'g'
+        )
+    ) AS name,
+    (
+        SELECT id
+        FROM countries
+        WHERE name = TRIM(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    CASE
+                        WHEN {{ source('core', 'raw_wfp') }}.adm0_name = 'Iran  (Islamic Republic of)' THEN 'Iran'
+                        WHEN {{ source('core', 'raw_wfp') }}.adm0_name = 'State of Palestine' THEN 'Palestine'
+                        ELSE {{ source('core', 'raw_wfp') }}.adm0_name
+                    END, '[^a-zA-Z0-9\s''\-\(\)]', '', 'g'),
+                '\s+', ' ', 'g'
+            )
+        )
+        LIMIT 1
+    ) AS country_id,
+    NULL::double precision AS latitude,
+    NULL::double precision AS longitude
 FROM {{ source('core', 'raw_wfp') }}
 WHERE adm1_id IS NOT NULL AND adm1_name IS NOT NULL
 ORDER BY name
