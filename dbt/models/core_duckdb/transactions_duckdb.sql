4b42395ccd78dd7205e871e1de5d41cd459f27fe
Andres Caceres <8844096+pinkfloydsito@users.noreply.github.com>
2024-11-24 20:44:41 +0200

    Duck integration. Issues with postgres

--- dbt/models/core_duckdb/transactions_duckdb.sql
+++ dbt/models/core_duckdb/transactions_duckdb.sql
@@ -1,4 +1,17 @@
 {{ config(materialized='table', schema='analytics') }}
 
+--  SELECT * FROM postgres_scan('public', 'transactions')
+{{ config(materialized='table', schema='analytics') }}
+
 SELECT *
-FROM {{ postgres_scan('public', 'transactions') }}
+FROM postgres_scan(
+    schema_name => 'public',
+    table_name => 'transactions',
+    host => '{{ var("postgres_host", "postgres") }}',
+    port => {{ var("postgres_port", 5432) }},
+    user => '{{ var("postgres_user", "airflow") }}',
+    password => '{{ var("postgres_password", "airflow") }}',
+    database => '{{ var("postgres_database", "market_flow") }}'
+)
+
+-- select * from {{ ref('transactions') }}
